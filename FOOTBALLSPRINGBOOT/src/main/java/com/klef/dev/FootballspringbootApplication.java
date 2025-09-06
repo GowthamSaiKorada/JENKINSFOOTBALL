cdf6ddff431184919ab68ef29c81edad3cd69df9
GowthamSaiKorada <2300032467@kluniversity.in>
2025-09-06 07:44:28 +0530

    modified FootballsprinbootApplication.java

--- FOOTBALLSPRINGBOOT/src/main/java/com/klef/dev/FootballspringbootApplication.java
+++ FOOTBALLSPRINGBOOT/src/main/java/com/klef/dev/FootballspringbootApplication.java
@@ -2,11 +2,18 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FootballspringbootApplication extends SpringBootServletInitializer
 {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // This tells external Tomcat what the primary Spring Boot source is
+        return application.sources(FootballspringbootApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FootballspringbootApplication.class, args);
         System.out.println("Football Player Spring Boot Project is Running ...");
